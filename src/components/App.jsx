ed7dcfd5d0bacb67996723b757f1f0aa49d9009b
Olia Rosiak <olia.rosiak@gmail.com>
2023-05-18 21:48:03 +0300

    add profile component / user to app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import user from '../user.json';
+import Profile from "./profile/Profile";
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +14,13 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
