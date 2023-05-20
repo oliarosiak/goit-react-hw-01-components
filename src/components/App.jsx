599e285269766e523d556a9a227101d8d793d1c2
Olia Rosiak <olia.rosiak@gmail.com>
2023-05-20 21:28:34 +0300

    add friend + transactions + upload app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
 import user from '../user.json';
 import data from '../data.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
 import Profile from "./profile/Profile";
 import Statistics from './statistics/Statistics';
+import FriendList from './friendList/FriendList';
+import TransactionHistory from './transactionHistory/TransactionHistory';
+
 
 console.log(data);
+console.log(friends);
+console.log(transactions);
 
 export const App = () => {
   return (
@@ -27,6 +34,10 @@ export const App = () => {
 
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
+
+      <FriendList friends={friends} />;
+
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
