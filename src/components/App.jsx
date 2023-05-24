16081237c125b3dc4406c8c561921a8115957d3b
Olia Rosiak <olia.rosiak@gmail.com>
2023-05-24 11:22:02 +0300

    update App( )
    changing the path to the imported files thanks to jsconfig.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import user from '../user.json';
-import data from '../data.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+import user from 'user.json';
+import data from 'data.json';
+import friends from 'friends.json';
+import transactions from 'transactions.json';
 
-import Profile from "./profile/Profile";
-import Statistics from './statistics/Statistics';
-import FriendList from './friendList/FriendList';
-import TransactionHistory from './transactionHistory/TransactionHistory';
+import Profile from "components/profile/Profile";
+import Statistics from 'components/statistics/Statistics';
+import FriendList from 'components/friendList/FriendList';
+import TransactionHistory from 'components/transactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
